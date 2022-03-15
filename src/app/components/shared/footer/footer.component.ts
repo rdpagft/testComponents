d00dc0d78662c051f8ff352ff77a656cd08d458d
Jose Daniel Contreras Medina <jeni@gft.com>
2022-03-15 14:22:53 -0600

    titulo en navbar dinamico y opciones agregadas en la pantalla de administracion en formato mobile

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError} from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
   mostrarMenuRestante: boolean;
-  constructor() {
+  currentRoute: string;
+  subscription !: Subscription;
+  constructor(private router: Router) {
     this.mostrarMenuRestante = false
+    this.currentRoute = "";
+
+  
+  }
+  
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
   }
 
+  ngOnInit(): void { 
+  }
 
-  ngOnInit(): void {
+  getRoute(){
+    this.subscription = this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        // Show loading indicator
+        console.log('Route change detected');
+      }
+
+      if (event instanceof NavigationEnd) {
+        // Hide loading indicator
+        this.currentRoute = event.url;          
+          console.log(event);
+      }
+    });
   }
   mostrarOpcionesRestantes(){
     console.log("mostrar menu");
