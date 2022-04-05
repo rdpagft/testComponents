4a6414a7480f4da96a58b93622e8fb2f8b4d50bc
Jose Daniel Contreras Medina <jeni@gft.com>
2022-04-04 19:48:42 -0500

    input de busqueda - faltan los estiles

--- src/app/components/shared/google-maps/google-maps.component.ts
+++ src/app/components/shared/google-maps/google-maps.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-google-maps',
   templateUrl: './google-maps.component.html',
   styleUrls: ['./google-maps.component.scss']
 })
-export class GoogleMapsComponent implements OnInit {
+export class GoogleMapsComponent implements OnInit, AfterViewInit{
+
+  @ViewChild(GoogleMap) map !: GoogleMap
+  @ViewChild('mapSearchField') mapSearchField !: ElementRef
+  @ViewChild('container__map') container__map !: ElementRef
 
   @Input() options !: google.maps.MapOptions 
   @Input() center !: google.maps.LatLngLiteral
@@ -13,6 +18,27 @@ export class GoogleMapsComponent implements OnInit {
 
 
   constructor() { }
+  ngAfterViewInit(): void {
+
+    // setTimeout(() => {
+      console.log(this.mapSearchField);
+     const searchBox = new google.maps.places.SearchBox(this.mapSearchField.nativeElement)
+     this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+      this.mapSearchField.nativeElement
+     )
+     searchBox.addListener('places_changed', ()=>{
+       const places =searchBox.getPlaces()
+       if(places?.length === 0) return
+       const bounds  = new google.maps.LatLngBounds()
+       places?.forEach(place => {
+          if(!place.geometry || !place.geometry.location) return
+          place.geometry.viewport ? bounds.union(place.geometry.viewport) : bounds.extend(place.geometry.location)
+       })
+       this.map.fitBounds(bounds)
+     })
+      
+    // }, 1000);
+  }
 
 
   ngOnInit(): void {
@@ -22,4 +48,5 @@ export class GoogleMapsComponent implements OnInit {
 
   }
 
+  
 }
